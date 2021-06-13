9f4c0e4aeb96292932bd8e20fa82254dbf987591
frost-oak <qzc_master@163.com>
2021-06-13 20:56:51 +0800

    A nodejs back-end server

---
+++ config/wxconfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  AppID: '',
+  AppSecret: '',
+  // 商户id
+  MchId: '1588888888',
+  // 商户支付key
+  PayKey: 'f5c8e398d51c2661da53f2c426505c99',
+  wxPayNotifiyUrl: 'http://127.0.0.1'
+}
